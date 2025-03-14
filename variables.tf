4febcdb75e87185cb8e5af81f46d335d1b258c06
Algot Sarika <sarika@joindevops.com>
2025-03-14 16:07:30 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -26,3 +26,78 @@ variable "vpc_tags" {
     default = {}
   
 }
+
+variable "igw_tags" {
+    default = {}
+  
+}
+
+variable "public_subnet_cidirs" {
+    type = list
+    validation {
+        condition     = length(var.public_subnet_cidirs) == 2
+        error_message = "please provide 2 vaild public subnet CIDR"
+    }
+
+}
+
+variable "public_subnet_tags" {
+    default = {}
+  
+}
+
+variable "private_subnet_cidirs" {
+    type = list
+    validation {
+        condition     = length(var.private_subnet_cidirs) == 2
+        error_message = "please provide 2 vaild private subnet CIDR"
+    }
+
+}
+
+variable "private_subnet_tags" {
+    default = {}
+  
+}
+
+variable "database_subnet_cidirs" {
+    type = list
+    validation {
+        condition     = length(var.database_subnet_cidirs) == 2
+        error_message = "please provide 2 vaild database subnet CIDR"
+    }
+
+}
+
+variable "database_subnet_tags" {
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    default = {}
+  
+}
+
+variable "private_route_table_tags" {
+    default = {}
+  
+}
+
+variable "database_route_table_tags" {
+    default = {}
+  
+}
+
+variable "is_peering_required" {
+    default = false
+  
+}
+
+variable "vpc_peering_tags" {
+    default = {}
+  
+}
